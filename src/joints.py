88512a81417ad7889747b72f628f38405f6ec8bb
KevinQ7 <s2266187@ed.ac.uk>
2021-11-16 16:30:55 +0000

    finished  joints.py

--- src/joints.py
+++ src/joints.py
@@ -6,13 +6,20 @@ import numpy as np
 from std_msgs.msg import  Float64
 
 def publisher():
-    rospy.init_node('joint1_publisher', anonymous=True)
-    pub1 = rospy.Publisher("/robot/joint2_position_controller/command", Float64, queue_size = 10)
+    rospy.init_node('joints_publisher', anonymous=True)
+    pub2 = rospy.Publisher("/robot/joint2_position_controller/command", Float64, queue_size = 10)
+    pub3 = rospy.Publisher("/robot/joint3_position_controller/command", Float64, queue_size = 10)
+    pub4 = rospy.Publisher("/robot/joint4_position_controller/command", Float64, queue_size = 10)
 
     while not rospy.is_shutdown():
         t = rospy.get_time()
-        joint1_pos = np.pi /2 * np.sin(np.pi/15 *t)
-        pub1.publish(joint1_pos)
+        joint2 = np.pi /2 * np.sin(np.pi/15 *t)
+        joint3 = np.pi /2 * np.sin(np.pi/20 *t)
+        joint4 = np.pi /2 * np.sin(np.pi/18 *t)
+
+        pub2.publish(joint2)
+        pub3.publish(joint3)
+        pub4.publish(joint4)
 
 
 if __name__ == '__main__':

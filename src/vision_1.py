aee2433db54da4781a59e71abf9c741c69705054
belindajin <58836051+belindajin@users.noreply.github.com>
2021-12-02 10:30:39 +0000

    syntax fix

--- src/vision_1.py
+++ src/vision_1.py
@@ -113,14 +113,11 @@ class image_converter:
     newX = np.cross(yellowBlue, np.array([0, 1, 0]))
 
     # find angle of joints
-    joint2 = np.arccos((np.dot(newX, np.array([1, 0, 0]))) /
-        (np.linalg.norm(newX) + np.linalg.norm(np.array([1, 0, 0])))
+    joint2 = np.arccos((np.dot(newX, np.array([1, 0, 0]))) / (np.linalg.norm(newX) + np.linalg.norm(np.array([1, 0, 0])))
 
-    joint3 = np.arccos((np.dot(yellowBlue, np.array([0, 1, 0]))) /
-        (np.linalg.norm(yellowBlue) + np.linalg.norm(np.array([0, 1, 0]))))
+    joint3 = np.arccos((np.dot(yellowBlue, np.array([0, 1, 0]))) / (np.linalg.norm(yellowBlue) + np.linalg.norm(np.array([0, 1, 0]))))
 
-    joint4 = np.arccos((np.dot(yellowBlue, blueRed)) /
-        (np.linalg.norm(yellowBLue) + np.linalg.norm(blueRed)))
+    joint4 = np.arccos((np.dot(yellowBlue, blueRed)) / (np.linalg.norm(yellowBLue) + np.linalg.norm(blueRed)))
 
     return [joint2, joint3, joint4]
 

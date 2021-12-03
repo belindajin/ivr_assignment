71dc00114e17db9b5c695422aea0161461c51719
belindajin <58836051+belindajin@users.noreply.github.com>
2021-12-03 06:57:12 +0000

    typos

--- src/vision_2.py
+++ src/vision_2.py
@@ -170,7 +170,7 @@ class image_converter:
         joint4 = -joint4
 
     yellowBlue2D = np.array([yellowBlue[0], yellowBlue[1]])
-    joint1 = np.arccos((np.dot(np.array(yellowBlue2D, np.array([0, 1]))) / (np.linalg.norm(yellowBlue2D) * np.linalg.norm(np.array([0, 1])))))
+    joint1 = np.arccos((np.dot(yellowBlue2D, np.array([0, 1]))) / (np.linalg.norm(yellowBlue2D) * np.linalg.norm(np.array([0, 1]))))
     print(joint1)
 
     return [joint1, joint3, joint4]

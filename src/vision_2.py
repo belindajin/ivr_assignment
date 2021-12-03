9ea26bee32a776ec8ce2072d02d5aaa7a0a3257c
belindajin <58836051+belindajin@users.noreply.github.com>
2021-12-03 13:42:03 +0000

    joint4 edits

--- src/vision_2.py
+++ src/vision_2.py
@@ -171,6 +171,8 @@ class image_converter:
 
     joint4 = np.arccos((np.dot(yellowBlue, blueRed)) / (np.linalg.norm(yellowBlue) * np.linalg.norm(blueRed)))
 
+    if joint4 > np.pi/2:
+        joint4 = joint4 - pi
     # if np.dot(blueRed, yellowBlue) < 0:
     #     print('something')
     #     joint4 = -joint4

b94ffabb0088527f7b3c9b1fbdd779fd65cd0998
belindajin <58836051+belindajin@users.noreply.github.com>
2021-12-03 06:32:38 +0000

    trying smtg w joint 4

--- src/vision_1.py
+++ src/vision_1.py
@@ -173,7 +173,8 @@ class image_converter:
 
     joint4 = np.arccos((np.dot(yellowBlue, blueRed)) / (np.linalg.norm(yellowBlue) * np.linalg.norm(blueRed)))
 
-
+    if np.dot(blueRed, newX) < 0:
+        joint4 = -joint4
 
     return [joint2, joint3, joint4]
 
